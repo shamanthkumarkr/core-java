3d0e92c37372b12ddf8d480d3beb0d6c0ad2d270
shamanthkumarkr <107828737+shamanthkumarkr@users.noreply.github.com>
2022-06-23 01:56:51 +0530

    Add files via upload

---
+++ BookMyMovie.java
@@ -0,0 +1,86 @@
+class BookMyMovie
+{
+	String movies[] = {"om","jogi","yajamana","A","H2O","gaja","Dangala","Appu"};
+	static int ticketPrice =200;
+	int noOfTickets ;
+	String theatreName ;
+	String movieName ;
+	int totalNoOfTickets ;
+	int totalOfticketPrice;
+	
+	String snacks[] ={"chips","kurkure","popcorn","gobi","CoolDrinks","Sweetcorn"};
+	static int snackPrice = 150;
+	String snackName;
+	int quantity ;
+	int seatNo ;
+	int totalNoOfSnacks ;
+	int toatalSnacksPrice;
+	
+	public BookMyMovie(String theatreName,int noOfTickets,String movieName,String snackName,int quantity,int seatNo)
+	{
+		System.out.println("BookMyMovie object is created");
+	  this.theatreName = theatreName;
+	  this.noOfTickets = noOfTickets;
+	  this.movieName = movieName;
+	   
+	  this.snackName = snackName;
+	  this.quantity = quantity;
+	  this.seatNo = seatNo;
+	}
+	 
+	public int showTime(int noOfTickets,String bookedBy,String movieName,int ticketPrice)
+	{
+	int totalOfTicketPrice =0;
+		System.out.println("inside showTime()...");
+		System.out.println("Arg 1 : No of tickets "+noOfTickets);
+		System.out.println("Arg 2 : Booked by "+bookedBy);
+		System.out.println("Arg 3 : Movie Name "+ movieName);
+		System.out.println("Arg 4 : Ticket price "+ ticketPrice);
+		
+		for(int i=0; i<movies.length ; i++)
+		{
+		if(movieName == movies[i])
+		{
+		System.out.println("Movie name matched") ;
+		
+		if(noOfTickets<totalNoOfTickets)
+		{
+		totalOfticketPrice=noOfTickets * ticketPrice;
+		totalNoOfTickets=totalNoOfTickets - noOfTickets;
+		System.out.println("For Movie : "+movieName + "No.of tickets are that are remaining "+totalNoOfTickets +"Total price is " +totalOfticketPrice );
+		}
+		else {System.out.println("ishtu tickets illa....");}
+		}
+	else {System.out.println("movie not Avialable");}
+   		}
+return totalOfTicketPrice;
+	}
+	public int buySnacks(String snackName , int quantity , int bookedSeatNo)
+	{
+		
+		int totalSnackPrice = 0;
+		 System.out.println("Arg 1 : Name of the Snack "+snackName);
+		 System.out.println("Arg 2 : Quantity of Snack "+quantity);
+		 System.out.println("Arg 3 : booked By seat No. "+bookedSeatNo);
+		 
+		 for(int z=0; z<snacks.length ; z++)
+		 {
+			 if(snackName==snacks[z])
+			 {
+			 System.out.println("The Snack Name is Matched");
+			 
+			 if(quantity<totalNoOfSnacks)
+			 { totalSnackPrice = quantity * snackPrice;
+				totalNoOfSnacks=totalNoOfSnacks - quantity;
+		       System.out.println(" For snacks : "+ quantity + " snacks remaining "+ totalNoOfSnacks +" total price is " + totalSnackPrice );
+		     }   
+		 else {System.out.println("kelavu snacks ella kaali agidave...");}
+		 }
+		 else {System.out.println("snack is Available");}
+	}
+	
+		return totalSnackPrice;
+	}
+	
+	
+}
---
+++ BookMyMoviesTester.java
@@ -0,0 +1,26 @@
+class BookMyMoviesTester{
+
+
+public static void main (String a[]){
+	String movies[] = {"om","jogi","yajamana","A","H2O","gaja","Dangala","Appu"};
+	String snacks[] ={"chips","kurkure","popcorn","gobi","CoolDrinks","Sweetcorn"};
+
+BookMyMovie bookMyShow = new BookMyMovie("Prasanna",10,"om","kurkure",5,55);
+
+bookMyShow.showTime(10,"Devi","om",200);
+bookMyShow.buySnacks("kurkure",5,55);
+
+System.out.println ("Theatre name is "+bookMyShow.theatreName);
+System.out.println ("Snacks name name is "+bookMyShow.snackName);
+System.out.println("The ticket price is "+BookMyMoviesTester.ticketPrice);
+
+
+BookMyMovie bookMyShow1 = new BookMyMovie("VEERESH",20,"appu","popcorn",6,115);
+bookMyShow1.showTime(20,"shamanth","Appu",300);
+bookMyShow1.buySnacks("popcorn",6,115);
+System.out.println ("Theatre name is "+bookMyShow1.theatreName);
+System.out.println ("Snacks name name is "+bookMyShow1.snackName);
+
+}
+
+}
