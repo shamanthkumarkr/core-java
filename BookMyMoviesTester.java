568b9beff5103bc6ac948f04f7e696902240cf49
shamanthkumarkr <107828737+shamanthkumarkr@users.noreply.github.com>
2022-06-23 01:10:53 +0530

    Update BookMyMoviesTester.java

--- BookMyMoviesTester.java
+++ BookMyMoviesTester.java
@@ -1,18 +1,21 @@
 class BookMyMoviesTester{
 String movies[] = {"om","jogi","yajamana","A","H2O","gaja","Dangala","Appu"};
+String snacks[] ={"chips","kurkure","popcorn","gobi","CoolDrinks","Sweetcorn"};
+  static int ticketPrice =200;
+  
 public static void main (String a[]){
-
 BookMyMovie bookMyShow = new BookMyMovie("Prasanna",10,"om","kurkure",5,55);
+bookMyShow.showTime(10,"Devi","om");
+bookMyShow.buySnacks("kurkure",5,55);
 System.out.println ("Theatre name is "+bookMyShow.theatreName);
 System.out.println ("Snacks name name is "+bookMyShow.snackName);
 
-bookMyShow.showTime(10,"Devi","om");
-bookMyShow.buySnacks("kurkure",5,55);
-bookMyShow.showTime(20,"shamanth","Appu");
-
-bookMyShow.buySnacks("popcorn",8,115);
-
-
+BookMyMovie bookMyShow1 = new BookMyMovie("VEERESH",20,"gaja","popcorn",6,115);
+bookMyShow1.showTime(20,"shamanth","Appu");
+bookMyShow1.buySnacks("popcorn",6,115);
+System.out.println ("Theatre name is "+bookMyShow1.theatreName);
+System.out.println ("Snacks name name is "+bookMyShow1.snackName);
+System.out.println("The ticket price is "+BookMyMoviesTester.ticketPrice);
 
 }
 
